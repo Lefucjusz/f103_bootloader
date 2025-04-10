f33487d04bf1f8b8ebf6d302bcb1bdf28ade352e
Marcin Lyda <elektromarcin@gmail.com>
2025-04-10 14:00:20 +0200

    CMake config for libopencm3
    Added CMake configuration for libopencm3
    and STM32F103 MCU. Builds without errors,
    but the output binary was not tested yet.

---
+++ cmake/stm32f103.cmake
@@ -0,0 +1,26 @@
+set(OPENCM3_ROOT_DIR ${CMAKE_SOURCE_DIR}/third-party/libopencm3)
+
+# Add target to build libopencm3 for the target that will be used
+add_custom_target(libopencm3 make -j$(nproc) TARGETS=stm32/f1 WORKING_DIRECTORY ${OPENCM3_ROOT_DIR})
+
+# Create a specific CPU target
+add_library(stm32f103 STATIC IMPORTED)
+set_property(TARGET stm32f103 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENCM3_ROOT_DIR}/include)
+set_property(TARGET stm32f103 PROPERTY IMPORTED_LOCATION ${OPENCM3_ROOT_DIR}/lib/libopencm3_stm32f1.a)
+add_dependencies(stm32f103 libopencm3)
+target_link_directories(stm32f103 INTERFACE ${OPENCM3_ROOT_DIR}/lib)
+
+target_compile_definitions(stm32f103 INTERFACE -DSTM32F1)
+
+set(COMPILE_OPTIONS
+    --static
+    -nostartfiles
+    -fno-common
+    -mcpu=cortex-m3
+    -mfloat-abi=soft
+    -Wl,--print-memory-usage
+    -Wl,-Map=${CMAKE_PROJECT_NAME}.map
+)
+
+target_compile_options(stm32f103 INTERFACE ${COMPILE_OPTIONS})
+target_link_options(stm32f103 INTERFACE ${COMPILE_OPTIONS})
