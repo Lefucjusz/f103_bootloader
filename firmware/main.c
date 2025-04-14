5a46c339a997a6dcb99ff0cc3ae996779bedc89b
Marcin Lyda <elektromarcin@gmail.com>
2025-04-14 12:22:12 +0200

    More complex firmware, restructure project
    * Created more complex example firmware,
    blinking an LED and sending snprintf'd
    message via UART.
    * Restructured the project, moving
    all drivers and helpers to 'common'
    directory.

--- firmware/main.c
+++ firmware/main.c
@@ -1,23 +1,48 @@
+#include <system.h>
+#include <uart.h>
+#include <timer.h>
+#include <string.h>
+#include <stdio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <system.h>
+
+static void led_init(void)
+{
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+}
+
+static void led_toggle(void)
+{
+    gpio_toggle(GPIOC, GPIO13);
+}
 
 int main(void)
 {
     system_init();
+    led_init();
+    uart_init();
 
-    rcc_periph_clock_enable(RCC_GPIOC);
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+    struct timer_t led_timer;
+    struct timer_t uart_timer;
 
+    timer_init(&led_timer, 250);
+    timer_init(&uart_timer, 1000);
+
+    char msg_buffer[64];
+    size_t msg_counter = 0;
 
     while (1) {
-        for (int i = 50; i < 500; i += 20) {
-            gpio_toggle(GPIOC, GPIO13);
-            system_delay_ms(i);
+        if (timer_has_elapsed(&led_timer)) {
+            led_toggle();
+            timer_reset(&led_timer);
         }
-        for (int i = 500; i >= 50; i -= 20) {
-            gpio_toggle(GPIOC, GPIO13);
-            system_delay_ms(i);
+
+        if (timer_has_elapsed(&uart_timer)) {
+            snprintf(msg_buffer, sizeof(msg_buffer), "Hello World %u from signed binary!\n", msg_counter);
+            ++msg_counter;
+            uart_write(msg_buffer, strlen(msg_buffer));
+            timer_reset(&uart_timer);
         }
     }
 }
